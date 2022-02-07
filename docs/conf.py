c9a5386f3701b98fd7305d34fe552750849e959d
manuel.gloeckler@student.uni-tuebingen.de <manuel.gloeckler@student.uni-tuebingen.de>
2022-02-07 17:32:04 +0100

    change index

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath(f'..{os.path.sep}src'))
 
 
 # -- Project information -----------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -2,7 +2,5 @@ Welcome to helloWorld's documentation!
 ======================================
  
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
  
   autodoc
